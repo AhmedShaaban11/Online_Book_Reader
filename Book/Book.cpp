4ee8c39feefa82dc8808ca6e74bd55798229332e
Ahmed Shaaban <ahmedlearningscience@gmail.com>
2023-02-07 13:45:57 +0200

    Added Book class data members and methods

--- Book/Book.cpp
+++ Book/Book.cpp
@@ -2,4 +2,24 @@
 // Created by ahmed on 2/7/2023.
 //
 
+#include <iostream>
 #include "Book.h"
+#include "../Namespaces/gpm.h"
+
+using std::cout;
+
+Book::Book(const string &title) : title_{title} {
+
+}
+
+string Book::GetTitle() const {
+  return title_;
+}
+
+void Book::Print() const {
+  cout << "Title: " << title_ << "\n";
+}
+
+string Book::ToString(const string &del) const {
+  return gpm::ToString(del, title_);
+}
--- Book/Book.h
+++ Book/Book.h
@@ -5,9 +5,18 @@
 #ifndef ONLINE_BOOK_READER_BOOK_H
 #define ONLINE_BOOK_READER_BOOK_H
 
+#include <string>
+
+using std::string;
 
 class Book {
-
+ private:
+  string title_;
+ public:
+  explicit Book(const string &title);
+  string GetTitle() const;
+  void Print() const;
+  string ToString(const string &del = ",") const;
 };
 
 
