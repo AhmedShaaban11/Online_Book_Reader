cdb42098bff4d819e1a6dd529d1dd5f59cce2228
Ahmed Shaaban <ahmedlearningscience@gmail.com>
2023-02-09 11:33:02 +0200

    Added Profile Feature

--- App/App.cpp
+++ App/App.cpp
@@ -19,12 +19,14 @@ App::App(const string &users_path, const string &books_path,
   sign_menu_.emplace_back("SignUp");
   sign_menu_.emplace_back("LogIn");
   sign_menu_.emplace_back("Exit");
+  admin_menu_.emplace_back("Profile");
+  admin_menu_.emplace_back("Add Book");
+  admin_menu_.emplace_back("LogOut");
+  customer_menu_.emplace_back("Profile");
   customer_menu_.emplace_back("Read");
   customer_menu_.emplace_back("History");
   customer_menu_.emplace_back("List of Books");
   customer_menu_.emplace_back("LogOut");
-  admin_menu_.emplace_back("Add Book");
-  admin_menu_.emplace_back("LogOut");
   session_menu_.emplace_back("Print Current Page");
   session_menu_.emplace_back("Go To The Next Page");
   session_menu_.emplace_back("Go To The Previous Page");
@@ -79,6 +81,8 @@ bool App::RunAdmin() {
   gpm::PrintSeparator('-', 15);
   int c = gpm::InputInt("Enter a number:", 0, (int) admin_menu_.size() - 1);
   if (c == 0) {
+    usr_->Print();
+  } else if (c == 1) {
     books_.AddBook();
   } else {
     LogOut();
@@ -91,13 +95,15 @@ bool App::RunCustomer() {
   gpm::PrintSeparator('-', 15);
   int c = gpm::InputInt("Enter a number:", 0, (int) customer_menu_.size() - 1);
   if (c == 0) {
+    usr_->Print();
+  } else if (c == 1) {
     string title = books_.AccessBook();
     if (title.empty()) { return true; }
     sess_ = sessions_.AccessSession(title, usr_->GetUsername());
     sess_->Open(books_dir_ + title);
-  } else if (c == 1) {
-    sessions_.PrintHistory(usr_->GetUsername());
   } else if (c == 2) {
+    sessions_.PrintHistory(usr_->GetUsername());
+  } else if (c == 3) {
     books_.PrintBooks();
   } else {
     LogOut();
